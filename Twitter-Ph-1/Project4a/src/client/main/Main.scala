0d4e6f42effa14ea188dde5395b9610a61fd26a9
unknown <bhavneshgugnani>
2014-11-23 00:23:27 -0500

    Commit 2

--- Twitter-Ph-1/Project4a/src/client/main/Main.scala
+++ Twitter-Ph-1/Project4a/src/client/main/Main.scala
@@ -7,10 +7,11 @@ import client.actor.ClientActor
 object Main {
 
   def main(args: Array[String]) {
+    
     val followers = Array(8, 7, 7, 5, 5, 3, 3, 1, 1, 1)
     val numberOfTweetsPerDay = Array(9, 4, 3, 2, 2, 1, 1, 1, 1, 0)
-    val clients: Int = 284000000
-    val sampleSize: Int = 8
+    val clients: Int = 10 //284000000
+    val sampleSize: Int = 10
     val hostAddress: String = args(0)
     val TwitterServerPort = 4030
     val serverAddress: String = "akka.tcp://Project4aServer@" + hostAddress + ":" + TwitterServerPort + "/user/ProcessingRouter" //args(0)
@@ -19,7 +20,7 @@ object Main {
     val system = ActorSystem("Project4aClient")
 
     for (i <- 1 to clients) {
-      var client = system.actorOf(Props(new ClientActor(serverAddress, followers(i) % sampleSize, numberOfTweetsPerDay(i) / sampleSize, i * offset)), "Client" + i)
+      var client = system.actorOf(Props(new ClientActor(serverAddress, followers((i % sampleSize) - 1), numberOfTweetsPerDay((i % sampleSize) - 1), i * offset)), "Client" + i)
     }
   }
 }
