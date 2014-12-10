5f902a68c7146cde12285de23eb9665fe56c3900
patyogesh <yogeshpatil20@ufl.edu>
2014-12-09 22:15:09 -0500

    Removed copy

--- Twitter-Ph-1/Project4b/src/main/scala/common/Constants.scala
+++
@@ -1,7 +0,0 @@
-package common
-
-//#Contains various constants used in the project.  
-class Constants {
-  val SERVER_PORT: Int = 4032
-  val UPDATE_TIMEOUT: Int = 2000
-}
--- Twitter-Ph-1/Project4b/src/main/scala/common/Tweet.scala
+++
@@ -1,9 +0,0 @@
-package main.scala.common
-
-//#Stores information of a tweet on server side.
-class Tweet(uid: String, tweettext: String, username: String) {
-
-  val uuid: String = uid
-  val text: String = tweettext
-  val userName: String = username
-}
