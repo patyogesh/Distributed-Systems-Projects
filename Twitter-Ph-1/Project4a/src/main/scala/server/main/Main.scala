bd0ca0fbf8487f049667174dd27a488726f455bf
patyogesh <yogeshpatil20@ufl.edu>
2014-12-11 00:09:06 -0500

    checking work space

--- Twitter-Ph-1/Project4a/src/main/scala/server/main/Main.scala
+++ Twitter-Ph-1/Project4a/src/main/scala/server/main/Main.scala
@@ -21,6 +21,7 @@ import scala.collection.convert.decorateAsScala._
 //#This class is the main class to launch the tweeter server for this project. 
 object Main {
   def main(args: Array[String]) {
+    println("YOGESH BHAI")
     val constants = new Constants()
 
     val hostAddress: String = java.net.InetAddress.getLocalHost.getHostAddress()
