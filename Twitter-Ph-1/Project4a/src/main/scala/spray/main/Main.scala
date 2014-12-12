0c3f0293cb59136a001bc35c826b42f165f8fc00
unknown <bhavneshgugnani>
2014-12-12 18:51:11 -0500

    Spray listening to REST request

--- Twitter-Ph-1/Project4a/src/main/scala/spray/main/Main.scala
+++ Twitter-Ph-1/Project4a/src/main/scala/spray/main/Main.scala
@@ -18,19 +18,20 @@ object Main {
     val akkaServerIP = args(0)
     val localAddress: String = java.net.InetAddress.getLocalHost.getHostAddress()
     val constants = new Constants()
-    val port = constants.SERVER_PORT 
+    val port = constants.SERVER_PORT
     val cores: Int = Runtime.getRuntime().availableProcessors();
-    
-    
+
     val requestMap: concurrent.Map[String, ActorRef] = new ConcurrentHashMap().asScala
-    
+
     implicit val system = ActorSystem("SprayServer")
 
     // the handler actor replies to incoming HttpRequests
     var handler: ActorRef = null
-    for (i <- 1 to 2 * cores) {
+    handler = system.actorOf(Props(new RequestListenerService("RequestListener", akkaServerIP, localAddress, port, requestMap)), name = "RequestListener")
+    IO(Http) ! Http.Bind(handler, interface = localAddress, port = port)
+    /*for (i <- 1 to 2 * cores) {
       handler = system.actorOf(Props(new RequestListenerService("RequestListener" + i, akkaServerIP, localAddress, port, requestMap)), name = "RequestListener" + i)
       IO(Http) ! Http.Bind(handler, interface = localAddress, port = port)
-    }
+    }*/
   }
 }
