d2d0a6347e337236fa11e31bb12f0a106b46268c
unknown <bhavneshgugnani>
2014-12-14 19:11:26 -0500

    Router working for spray server

--- Twitter-Ph-1/Project4a/src/main/scala/spray/server/main/Main.scala
+++ Twitter-Ph-1/Project4a/src/main/scala/spray/server/main/Main.scala
@@ -1,12 +1,9 @@
 package main.scala.spray.server.main
 
 import java.util.concurrent.ConcurrentHashMap
-
 import scala.collection.concurrent
 import scala.collection.convert.decorateAsScala.mapAsScalaConcurrentMapConverter
-
 import com.typesafe.config.ConfigFactory
-
 import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
@@ -15,6 +12,7 @@ import akka.io.IO
 import main.scala.common.Constants
 import main.scala.spray.server.actor.service.impl.RequestListenerService
 import spray.can.Http
+import main.scala.spray.server.actor.service.router.RequestListenerRouter
 
 object Main {
 
@@ -44,8 +42,8 @@ object Main {
     implicit val system = ActorSystem("SprayServer", ConfigFactory.load(configuration))
 
     // the handler actor replies to incoming HttpRequests
-    var handler: ActorRef = null
-    handler = system.actorOf(Props(new RequestListenerService("RequestListener", localAddress, constants.SPRAY_SERVER_PORT_FOR_AKKA_MESSAGES, akkaServerIP, constants.AKKA_SERVER_PORT, constants.followers, requestMap)), name = "RequestListener")
+    var handler: ActorRef = system.actorOf(Props(new RequestListenerRouter(2*cores, "RequestListenerRouter", localAddress, constants.SPRAY_SERVER_PORT_FOR_AKKA_MESSAGES, akkaServerIP, constants.AKKA_SERVER_PORT, constants.followers, requestMap)), name = "RequestListenerRouter")
+    //handler = system.actorOf(Props(new RequestListenerService("RequestListener", localAddress, constants.SPRAY_SERVER_PORT_FOR_AKKA_MESSAGES, akkaServerIP, constants.AKKA_SERVER_PORT, constants.followers, requestMap)), name = "RequestListener")
     IO(Http) ! Http.Bind(handler, interface = localAddress, port = constants.SPRAY_SERVER_PORT_FOR_HTTP_MESSAGES)
 
   }
